3b4e85550814240099c5f1c315df86745623f549
Prakshal Jain <prakshaljain7@gmail.com>
2024-07-27 20:48:31 +0530

    add case for different delimiter

--- calculator.js
+++ calculator.js
@@ -3,8 +3,14 @@ class Calculator {
         if (number_strings == "") {
             return 0;
         }
-        let new_str = number_strings.replace(/\n/g, ",");
-        const arr = new_str.split(",").map((item) => parseInt(item));
+        let new_str = number_strings;
+        let delimiter = ",";
+        if (number_strings.startsWith("//")) {
+            delimiter = number_strings.at(2);
+            new_str = new_str.substring(4);
+        }
+        new_str = new_str.replace(/\n/g, delimiter);
+        const arr = new_str.split(delimiter).map((item) => parseInt(item));
         return arr.reduce((acc, itr) => acc + itr);
     }
 }
