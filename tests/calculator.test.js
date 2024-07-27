e3bd3546538cecc002d2d21700d22d0ba3973da7
Prakshal Jain <prakshaljain7@gmail.com>
2024-07-27 19:58:00 +0530

    added test cases for sum of multiple numbers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,3 +4,13 @@ test("should return 0 for case of an empty string", () => {
     const calculator = new Calculator();
     expect(calculator.Add("")).toBe(0);
 });
+
+test("Should add two numbers provided in a string", () => {
+    const calculator = new Calculator();
+    expect(calculator.Add("2,5")).toBe(7);
+});
+
+test("Shouuld return sum of numbers in case of multiple numbers", () => {
+    const calculator = new Calculator();
+    expect(calculator.Add("3,4,7,10,11,14")).toBe(49);
+});
