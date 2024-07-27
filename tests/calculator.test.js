904908f349ca869bb3aeaa8a2daffcdac7775b81
Prakshal Jain <prakshaljain7@gmail.com>
2024-07-27 20:06:21 +0530

    added test case for different delimiter

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -20,3 +20,9 @@ test("Should return sum of numbers in case of new line added in string", () => {
     expect(calculator.Add("7\n10")).toBe(17);
     expect(calculator.Add("3,4,7\n10\n11,14")).toBe(49);
 });
+
+test("Should handle a different delimiter", () => {
+    const calculator = new Calculator();
+    expect(calculator.Add("//;\n1;2")).toBe(3);
+    expect(calculator.Add("//;\n3;4;7;10;11;14")).toBe(49);
+});
